81595fb0c2a69420e1f8bd22b7527c178734bd8c
A7med Nabil <an223602@gmail.com>
2025-05-14 19:37:50 +0300

    Update project constant

--- src/constants/constants.py
+++ src/constants/constants.py
@@ -11,3 +11,21 @@ LOGGING_FORMAT = (
     "[%(asctime)s] [%(levelname)s] [%(name)s:%(funcName)s:%(lineno)d] - %(message)s"
 )
 DATEFMT = "%Y-%m-%d %H:%M:%S"
+
+
+# DATA SCRIPT
+FRUIT_CATEGORIES = [
+    "fresh_peaches",
+    "fresh_pomegranates",
+    "fresh_strawberries",
+    "rotten_peaches",
+    "rotten_pomegranates",
+    "rotten_strawberries",
+]
+
+# Processing constant
+TEST_SIZE = 0.2
+R_S = 0
+
+# Visualization constant
+FIG_SIZE = (8, 8)

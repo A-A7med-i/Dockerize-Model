bc968c752664fe1d91e585ea386e1747370ed66e
A7med Nabil <an223602@gmail.com>
2025-06-02 21:17:21 +0300

    Refactor files and directories

--- template/project_structure.py
+++ template/project_structure.py
@@ -1,3 +1,4 @@
+from src.constants.constants import DIRECTORIES, FILES
 from dataclasses import dataclass
 from typing import List
 
@@ -17,46 +18,5 @@ class ProjectStructure:
 
     def __init__(self):
         """Initialize default project structure."""
-        self.directories = [
-            "models/checkpoints",
-            "data/raw/",
-            "data/processed/",
-            "src/constants",
-            "src/utils",
-            "src/data",
-            "src/processing",
-            "src/visualization",
-            "src/models",
-            "src/api",
-            "notebooks",
-        ]
-
-        self.files = [
-            "src/__init__.py",
-            "src/constants/constants.py",
-            "src/constants/__init__.py",
-            "src/utils/__init__.py",
-            "src/utils/helper.py",
-            "src/data/__init__.py",
-            "src/data/implement_data.py",
-            "src/processing/__init__.py",
-            "src/processing/processor.py",
-            "src/visualization/__init__.py",
-            "src/visualization/plot.py",
-            "src/models/__init__.py",
-            "src/models/model.py",
-            "src/api/__init__.py",
-            "src/api/endpoints.py",
-            "src/api/main.py",
-            "src/api/schemas.py",
-            "src/api/process.py",
-            "models/checkpoints/.gitkeep",
-            "requirements.txt",
-            "notebooks/EDA.ipynb",
-            "notebooks/experiments.ipynb",
-            "notebooks/exploratory.ipynb",
-            ".gitignore",
-            "README.md",
-            "LICENSE",
-            "setup.py",
-        ]
+        self.directories = DIRECTORIES
+        self.files = FILES
